3da9a94f1add9631f86e8e176bc711e3c8a492d6
Firzus <lprieu@gaming.tech>
2022-11-27 21:57:52 +0100

    fix map

--- game/ConfigMap.h
+++ game/ConfigMap.h
@@ -1,3 +1,4 @@
 #pragma once
 
-#define MAP_PATH "data/assets/tileMap/tileSet.png"
+#define TILE_MAP_PATH "data/assets/tileMap/tileSet.png"
+#define COLLISION_MAP_PATH "data/assets/tileMap/collision.png"
--- game/main.cpp
+++ game/main.cpp
@@ -22,35 +22,35 @@ int main()
     // on crée la tilemap
     TileMap Ocean, Island, Trees_1, Trees_2, Trees_3, Plateau, Flower_grass, Bushes, Fence, House_trees, House, Dock/*, Collisions*//*, Battle_zones*/, Foreground_objects;
 
-    if (!Ocean.load(MAP_PATH, sf::Vector2u(12, 12), ocean, 70, 40))
+    if (!Ocean.load(TILE_MAP_PATH, sf::Vector2u(12, 12), ocean, 70, 40))
         return -1;
-    if (!Island.load(MAP_PATH, sf::Vector2u(12, 12), island, 70, 40))
+    if (!Island.load(TILE_MAP_PATH, sf::Vector2u(12, 12), island, 70, 40))
         return -1;
-    if (!Trees_1.load(MAP_PATH, sf::Vector2u(12, 12), trees_1, 70, 40))
+    if (!Trees_1.load(TILE_MAP_PATH, sf::Vector2u(12, 12), trees_1, 70, 40))
         return -1;
-    if (!Trees_2.load(MAP_PATH, sf::Vector2u(12, 12), trees_2, 70, 40))
+    if (!Trees_2.load(TILE_MAP_PATH, sf::Vector2u(12, 12), trees_2, 70, 40))
         return -1;
-    if (!Trees_3.load(MAP_PATH, sf::Vector2u(12, 12), trees_3, 70, 40))
+    if (!Trees_3.load(TILE_MAP_PATH, sf::Vector2u(12, 12), trees_3, 70, 40))
         return -1;
-    if (!Plateau.load(MAP_PATH, sf::Vector2u(12, 12), plateau, 70, 40))
+    if (!Plateau.load(TILE_MAP_PATH, sf::Vector2u(12, 12), plateau, 70, 40))
         return -1;
-    if (!Flower_grass.load(MAP_PATH, sf::Vector2u(12, 12), flower_grass, 70, 40))
+    if (!Flower_grass.load(TILE_MAP_PATH, sf::Vector2u(12, 12), flower_grass, 70, 40))
         return -1;
-    if (!Bushes.load(MAP_PATH, sf::Vector2u(12, 12), bushes, 70, 40))
+    if (!Bushes.load(TILE_MAP_PATH, sf::Vector2u(12, 12), bushes, 70, 40))
         return -1;
-    if (!Fence.load(MAP_PATH, sf::Vector2u(12, 12), fence, 70, 40))
+    if (!Fence.load(TILE_MAP_PATH, sf::Vector2u(12, 12), fence, 70, 40))
         return -1;
-    if (!House_trees.load(MAP_PATH, sf::Vector2u(12, 12), house_trees, 70, 40))
+    if (!House_trees.load(TILE_MAP_PATH, sf::Vector2u(12, 12), house_trees, 70, 40))
         return -1;
-    if (!House.load(MAP_PATH, sf::Vector2u(12, 12), house, 70, 40))
+    if (!House.load(TILE_MAP_PATH, sf::Vector2u(12, 12), house, 70, 40))
         return -1;
-    if (!Dock.load(MAP_PATH, sf::Vector2u(12, 12), dock, 70, 40))
+    if (!Dock.load(TILE_MAP_PATH, sf::Vector2u(12, 12), dock, 70, 40))
         return -1;
-    //if (!Collisions.load(MAP_PATH, sf::Vector2u(12, 12), collisions, 70, 40))
+    //if (!Collisions.load(COLLISION_MAP_PATH, sf::Vector2u(12, 12), collisions, 70, 40))
     //    return -1;
-    //if (!Battle_zones.load(MAP_PATH, sf::Vector2u(12, 12), battle_zones, 70, 40))
+    //if (!Battle_zones.load(TILE_MAP_PATH, sf::Vector2u(12, 12), battle_zones, 70, 40))
     //    return -1;
-    if (!Foreground_objects.load(MAP_PATH, sf::Vector2u(12, 12), foreground_objects, 70, 40))
+    if (!Foreground_objects.load(TILE_MAP_PATH, sf::Vector2u(12, 12), foreground_objects, 70, 40))
         return -1;
 
     MainCharacter mainCharacter = MainCharacter(texture);
@@ -71,7 +71,9 @@ int main()
         }
         countFrame++;
 
-        // draw map
+        // build map
+
+        // core
         window.drawMap(Ocean);
         window.drawMap(Island);
         window.drawMap(Trees_1);
@@ -79,15 +81,23 @@ int main()
         window.drawMap(Trees_3);
         window.drawMap(Plateau);
         window.drawMap(Flower_grass);
+
+        //// objects
         window.drawMap(Bushes);
         window.drawMap(Fence);
         window.drawMap(House_trees);
         window.drawMap(House);
         window.drawMap(Dock);
+
+        // debuging
         //window.drawMap(Collisions);
         //window.drawMap(Battle_zones);
-        window.drawMap(Foreground_objects);
+
+        //// character
         window.drawEntity(mainCharacter);
+
+        //// foreground objects
+        window.drawMap(Foreground_objects);
 
         // display window
         window.display();
