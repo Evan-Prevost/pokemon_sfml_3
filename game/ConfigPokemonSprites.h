ba3cf822ed02745be1b90e7c2c4cfc261dc96588
Evan-Prevost <eprevost@gaming.tech>
2022-12-01 15:47:54 +0100

    début combat

--- game/ConfigPokemonSprites.h
+++ game/ConfigPokemonSprites.h
@@ -2,7 +2,7 @@
 
 #define BULBIZARRE_TEXTURE_PATH "data/assets/pokemons/bulbizarre.png"
 //width,height,frames
-#define BULBIZARRE_SPRITE_SPEC{37,38,51}
+#define BULBIZARRE_SPRITE_SPEC {37,38,51}
 
 #define SALAMECHE_TEXTURE_PATH "data/assets/pokemons/salameche.png"
-#define SALAMECHE_SPRITE_SPEC{41,42,55}
+#define SALAMECHE_SPRITE_SPEC {41,42,55}
