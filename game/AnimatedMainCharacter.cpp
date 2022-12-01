cf93da246a5aea4dc389b5920157957e5b4b6d7a
Firzus <lprieu@gaming.tech>
2022-12-01 13:30:03 +0100

    add content

--- game/AnimatedMainCharacter.cpp
+++ game/AnimatedMainCharacter.cpp
@@ -12,7 +12,8 @@ AnimatedMainCharacter::AnimatedMainCharacter(const sf::Texture& texture, std::ve
 
     this->_sprite.setTextureRect(this->_animation_pos[this->_count][this->currentAnimation]);
 
-    this->boxCollision.setFillColor(sf::Color(100, 250, 50));
+    // uncomment to see box collision 
+    this->boxCollision.setFillColor(sf::Color(0, 0, 0, 50));
 }
 
 void AnimatedMainCharacter::nextAnimation(void)
