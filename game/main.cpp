e888b53e4348a992c8af85f6dfc4e6ab42e0d15b
Firzus <lprieu@gaming.tech>
2022-12-01 10:41:03 +0100

    fix

--- game/main.cpp
+++ game/main.cpp
@@ -13,15 +13,14 @@
 
 int main()
 {
-    GameWindow window;
+    GameWindow gameWindow;
 
     int countFrame = 0;
     int currentWindow = 0;
     bool gameOn = true;
     while (gameOn) {
-        if (currentWindow == 0) { //menu
-            
-
+        if (currentWindow == 0)
+        { //menu
             sf::Texture textureBG;
             if (!textureBG.loadFromFile(MENU_BACKGROUND_PATH))
                 return -1;
@@ -31,26 +30,27 @@ int main()
             if (!textureSB.loadFromFile(MENU_STARTBUTTON_PATH))
                 return -1;
             Entity start = Entity(textureSB);
-            start.setPosition(window.getSize().x / 2 - 50, window.getSize().y / 2);
+            start.setPosition(gameWindow.getSize().x / 2 - 50, gameWindow.getSize().y / 2);
 
 
-            while (window.isOpen() && currentWindow == 0) {
-                window.clear();
-                int result = window.handleEventsMenu();
+            while (gameWindow.isOpen() && currentWindow == 0) {
+                gameWindow.clear();
+                int result = gameWindow.handleEventsMenu();
                 switch (result) {
                 case 1:
                     gameOn = false;
                 case 2:
                     currentWindow = 1;
-                default :
-                    window.drawEntity(background);
-                    window.drawEntity(start);
-                    window.display();
+                default:
+                    gameWindow.drawEntity(background);
+                    gameWindow.drawEntity(start);
+                    gameWindow.display();
                 }
             }
         }
-        else if (currentWindow == 1) { //Game
-            window._view.setCenter(INITIAL_POS);
+        else if (currentWindow == 1)
+        { //Game
+            gameWindow._view.setCenter(INITIAL_POS);
 
             //character
             sf::Texture texture;
@@ -64,63 +64,63 @@ int main()
             if (!pauseTexture.loadFromFile("data/assets/pause.png"))
                 return -1;
             Entity pause = Entity(pauseTexture);
-            pause.setPosition(window._view.getCenter().x - pauseTexture.getSize().x / 2, window._view.getCenter().y - pauseTexture.getSize().y / 2);
+            pause.setPosition(gameWindow._view.getCenter().x - pauseTexture.getSize().x / 2, gameWindow._view.getCenter().y - pauseTexture.getSize().y / 2);
 
-    // on crée la tilemap
-    TileMap Ocean, Island, Trees_1, Trees_2, Trees_3, Plateau, Flower_grass, Bushes, Fence, House_trees, House, Dock, CollisionsVisible /*,Collisions*/ /*, Battle_zones*/, Foreground_objects;
+            // on crée la tilemap
+            TileMap Ocean, Island, Trees_1, Trees_2, Trees_3, Plateau, Flower_grass, Bushes, Fence, House_trees, House, Dock, CollisionsVisible /*,Collisions*/ /*, Battle_zones*/, Foreground_objects;
 
-    if (!Ocean.load(TILE_MAP_PATH, sf::Vector2u(12, 12), ocean, 70, 40))
-        return -1;
-    if (!Island.load(TILE_MAP_PATH, sf::Vector2u(12, 12), island, 70, 40))
-        return -1;
-    if (!Trees_1.load(TILE_MAP_PATH, sf::Vector2u(12, 12), trees_1, 70, 40))
-        return -1;
-    if (!Trees_2.load(TILE_MAP_PATH, sf::Vector2u(12, 12), trees_2, 70, 40))
-        return -1;
-    if (!Trees_3.load(TILE_MAP_PATH, sf::Vector2u(12, 12), trees_3, 70, 40))
-        return -1;
-    if (!Plateau.load(TILE_MAP_PATH, sf::Vector2u(12, 12), plateau, 70, 40))
-        return -1;
-    if (!Flower_grass.load(TILE_MAP_PATH, sf::Vector2u(12, 12), flower_grass, 70, 40))
-        return -1;
-    if (!Bushes.load(TILE_MAP_PATH, sf::Vector2u(12, 12), bushes, 70, 40))
-        return -1;
-    if (!Fence.load(TILE_MAP_PATH, sf::Vector2u(12, 12), fence, 70, 40))
-        return -1;
-    if (!House_trees.load(TILE_MAP_PATH, sf::Vector2u(12, 12), house_trees, 70, 40))
-        return -1;
-    if (!House.load(TILE_MAP_PATH, sf::Vector2u(12, 12), house, 70, 40))
-        return -1;
-    if (!Dock.load(TILE_MAP_PATH, sf::Vector2u(12, 12), dock, 70, 40))
-        return -1;
-    if (!CollisionsVisible.load(TILE_MAP_PATH, sf::Vector2u(12, 12), collisionsVisible, 70, 40))
-        return -1;
-    //if (!Collisions.load(TILE_MAP_PATH, sf::Vector2u(12, 12), collisions, 70, 40))
-    //    return -1;
-    //if (!Battle_zones.load(TILE_MAP_PATH, sf::Vector2u(12, 12), battle_zones, 70, 40))
-    //    return -1;
-    if (!Foreground_objects.load(TILE_MAP_PATH, sf::Vector2u(12, 12), foreground_objects, 70, 40))
-        return -1;
+            if (!Ocean.load(TILE_MAP_PATH, sf::Vector2u(12, 12), ocean, 70, 40))
+                return -1;
+            if (!Island.load(TILE_MAP_PATH, sf::Vector2u(12, 12), island, 70, 40))
+                return -1;
+            if (!Trees_1.load(TILE_MAP_PATH, sf::Vector2u(12, 12), trees_1, 70, 40))
+                return -1;
+            if (!Trees_2.load(TILE_MAP_PATH, sf::Vector2u(12, 12), trees_2, 70, 40))
+                return -1;
+            if (!Trees_3.load(TILE_MAP_PATH, sf::Vector2u(12, 12), trees_3, 70, 40))
+                return -1;
+            if (!Plateau.load(TILE_MAP_PATH, sf::Vector2u(12, 12), plateau, 70, 40))
+                return -1;
+            if (!Flower_grass.load(TILE_MAP_PATH, sf::Vector2u(12, 12), flower_grass, 70, 40))
+                return -1;
+            if (!Bushes.load(TILE_MAP_PATH, sf::Vector2u(12, 12), bushes, 70, 40))
+                return -1;
+            if (!Fence.load(TILE_MAP_PATH, sf::Vector2u(12, 12), fence, 70, 40))
+                return -1;
+            if (!House_trees.load(TILE_MAP_PATH, sf::Vector2u(12, 12), house_trees, 70, 40))
+                return -1;
+            if (!House.load(TILE_MAP_PATH, sf::Vector2u(12, 12), house, 70, 40))
+                return -1;
+            if (!Dock.load(TILE_MAP_PATH, sf::Vector2u(12, 12), dock, 70, 40))
+                return -1;
+            if (!CollisionsVisible.load(TILE_MAP_PATH, sf::Vector2u(12, 12), collisionsVisible, 70, 40))
+                return -1;
+            //if (!Collisions.load(TILE_MAP_PATH, sf::Vector2u(12, 12), collisions, 70, 40))
+            //    return -1;
+            //if (!Battle_zones.load(TILE_MAP_PATH, sf::Vector2u(12, 12), battle_zones, 70, 40))
+            //    return -1;
+            if (!Foreground_objects.load(TILE_MAP_PATH, sf::Vector2u(12, 12), foreground_objects, 70, 40))
+                return -1;
 
-            while (window.isOpen())
+            while (gameWindow.isOpen())
             {
-                if (window.isPause()) {
-                    window.handleEventsPause();
-                    int result = window.handleEventsMenu();
+                if (gameWindow.isPause()) {
+                    gameWindow.handleEventsPause();
+                    int result = gameWindow.handleEventsMenu();
                     switch (result) {
                     case 1:
                         gameOn = false;
                     case 2:
                         currentWindow = 1;
-                    default :
-                        pause.setPosition(window._view.getCenter().x - pauseTexture.getSize().x / 2, window._view.getCenter().y - pauseTexture.getSize().y / 2);
-                        window.drawEntity(pause);
+                    default:
+                        pause.setPosition(gameWindow._view.getCenter().x - pauseTexture.getSize().x / 2, gameWindow._view.getCenter().y - pauseTexture.getSize().y / 2);
+                        gameWindow.drawEntity(pause);
                     }
                 }
                 else {
-                    window.clear();
-                    window.handleEventsGame();
-                    int result = window.handleEventsMenu();
+                    gameWindow.clear();
+                    gameWindow.handleEventsGame();
+                    int result = gameWindow.handleEventsMenu();
                     switch (result) {
                     case 1:
                         gameOn = false;
@@ -128,10 +128,10 @@ int main()
                         currentWindow = 1;
                     default:
                         // camera folow character
-                        /*window._view(mainCharacter);*/
+                        /*gameWindow._view(mainCharacter);*/
 
                 // movement main character
-                        mainCharacter.handKeys(window._view);
+                        mainCharacter.handKeys(gameWindow._view);
 
                         if (countFrame % 15 == 0)
                         {
@@ -143,37 +143,38 @@ int main()
                         // build map
 
                         // core
-                        window.drawMap(Ocean);
-                        window.drawMap(Island);
-                        window.drawMap(Trees_1);
-                        window.drawMap(Trees_2);
-                        window.drawMap(Trees_3);
-                        window.drawMap(Plateau);
-                        window.drawMap(Flower_grass);
+                        gameWindow.drawMap(Ocean);
+                        gameWindow.drawMap(Island);
+                        gameWindow.drawMap(Trees_1);
+                        gameWindow.drawMap(Trees_2);
+                        gameWindow.drawMap(Trees_3);
+                        gameWindow.drawMap(Plateau);
+                        gameWindow.drawMap(Flower_grass);
 
                         //// objects
-                        window.drawMap(Bushes);
-                        window.drawMap(Fence);
-                        window.drawMap(House_trees);
-                        window.drawMap(House);
-                        window.drawMap(Dock);
+                        gameWindow.drawMap(Bushes);
+                        gameWindow.drawMap(Fence);
+                        gameWindow.drawMap(House_trees);
+                        gameWindow.drawMap(House);
+                        gameWindow.drawMap(Dock);
 
-            //// character
-            gameWindow.drawEntity(mainCharacter);
+                        //// character
+                        gameWindow.drawEntity(mainCharacter);
 
-            //// foreground objects
-            gameWindow.drawMap(Foreground_objects);
+                        //// foreground objects
+                        gameWindow.drawMap(Foreground_objects);
 
-            // Collisions
-            //gameWindow.drawMap(collisions);
+                        // Collisions
+                        //gameWindow.drawMap(collisions);
 
-            // debuging
-            gameWindow.drawMap(CollisionsVisible);
-            //gameWindow.drawMap(Battle_zones);
-        }
-        // display gameWindow
-        gameWindow.display();
+                        // debuging
+                        gameWindow.drawMap(CollisionsVisible);
+                        //gameWindow.drawMap(Battle_zones);
+                    }
+                    // display gameWindow
+                    gameWindow.display();
 
+                }
             }
         }
     }
